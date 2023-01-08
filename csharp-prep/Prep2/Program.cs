0c33972bf1a2f81d54d06ff11598263a0d70d405
tara-cannon <landsend@byui.edu>
2023-01-07 23:52:45 -0700

    Grading and Pass/Fail

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,41 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("Please enter grade percentage: ");
+        string userPercentage = Console.ReadLine();
+
+        int x = int.Parse(userPercentage);
+
+        if (x >= 90)
+        {
+            Console.WriteLine("A");
+        }
+        else if (x >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (x >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (x >= 60)
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("F");
+        }
+
+        if (x >= 70)
+        {
+            Console.WriteLine("Congratulations! You are passing your class.");
+        }
+        else
+        {
+            Console.WriteLine("Try harder next time.");
+        }
+
     }
 }
