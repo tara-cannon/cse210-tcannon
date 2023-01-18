aebb2bcd4cff0ba9590b2051c1219437596b6488
tara-cannon <landsend@byui.edu>
2023-01-17 22:51:16 -0700

    Jobs and Resume

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        //Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._company = "Our Livingroom";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2005;
+        job1._endYear = 2023;
+
+        //job1.DisplayJobDetails();
+
+        Job job2 = new Job();
+        job2._company = "The Kitchen";
+        job2._jobTitle = "Ice Cream Eater";
+        job2._startYear = 1984;
+        job2._endYear = 2035;
+
+        //job2.DisplayJobDetails();
+
+        Resume resume1 = new Resume();
+        resume1._name = "Tara Cannon";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        //Console.WriteLine(resume1._jobs[0]._jobTitle);
+
+        resume1.DisplayResumeDetails();
+
     }
 }
